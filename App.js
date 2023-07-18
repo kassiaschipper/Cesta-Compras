9c5c921c30c43b97914e579923d24b97ac2d65fb
Kássia <kassia_guterres@outlook.com>
2023-07-18 16:33:52 -0300

    Código refatorado

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar,SafeAreaView, View } from "react-native";
-import Cesta from "./src/telas/Cesta";
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from "@expo-google-fonts/montserrat";
+import * as SplashScreen from "expo-splash-screen";
+
+import mock from "./src/mocks/cesta"
+import Cesta from "./src/telas/Cesta";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontCarregada]  = useFonts ({
@@ -9,13 +14,14 @@ export default function App() {
   });
 
   if(!fontCarregada){
-    return <View></View>
+    return null
   }
-
+  
+  SplashScreen.hideAsync()
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock}/>
     </SafeAreaView>
   );
 }
